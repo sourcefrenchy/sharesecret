c164e83364b81126b76ffcb1bf5ebbf278ea282c
JMA <1182482+sourcefrenchy@users.noreply.github.com>
2021-04-13 09:22:41 -0400

    Captcha working when checking a link. This is ensure that bots (AVs scanners etc) are not reading the secret for the recipient.

--- sharesecret.go
+++ sharesecret.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"bufio"
-	"crypto/sha256"
+	"bytes"
 	_ "crypto/sha256"
-	"encoding/hex"
+	"crypto/tls"
+	"filippo.io/age"
 	"fmt"
+	rl "github.com/ahmedash95/ratelimit"
 	"github.com/dchest/captcha"
+	log "github.com/sirupsen/logrus"
+	"github.com/twinj/uuid"
 	"github.com/unrolled/secure"
-	"log"
+	"io"
 	"net"
 	"net/http"
 	"os"
@@ -16,9 +20,15 @@ import (
 	"text/template"
 )
 
+const (
+	fqdn = "localhost"
+	port = ":8443"
+)
+
 var (
-	m             = make(map[string]string)
-	noCaptchaList []string
+	m              = make(map[string]string)
+	noCaptchaList  []string
+	rateLimitation = rl.CreateLimit("5r/s")
 )
 
 // ContextIndex contains the Secret we will capture from the user
@@ -44,7 +54,13 @@ func readLines(path string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
+
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Printf("error closing file after reading: %v", err)
+		}
+	}()
 
 	var lines []string
 	scanner := bufio.NewScanner(file)
@@ -63,13 +79,30 @@ func isIpv4(in string) bool {
 	return true
 }
 
-func generateURL(secret string) string {
-	h := sha256.New()
-	h.Write([]byte(secret))
-	uniqueLink := hex.EncodeToString(h.Sum(nil))
-	m[uniqueLink] = secret
-	log.Printf("\nNew secret saved in memory fr %s", uniqueLink)
-	return uniqueLink
+func generateURL(ipaddr string, secret string) string {
+	uniqueLink := uuid.NewV4().String()
+	i, err := age.GenerateX25519Identity()
+	if err != nil {
+		log.Fatalf("Encryption failed: %v", err)
+	}
+	publicKey := i.Recipient()
+	privateKey := i.String()
+	out := &bytes.Buffer{}
+
+	w, err := age.Encrypt(out, publicKey)
+	if err != nil {
+		log.Fatalf("Cannot crearw encrypted file %v", err)
+	}
+	if _, err := io.WriteString(w, secret); err != nil {
+		log.Fatalf("Failed to write encrypted file %v", err)
+	}
+	if err := w.Close(); err != nil {
+		log.Fatalf("Failed to close encrypted file %v", err)
+	}
+	log.Printf("[*] New encrypted secret from %s saved in memory at %s", ipaddr, uniqueLink)
+
+	m[uniqueLink] = out.String()
+	return uniqueLink + "." + strings.ReplaceAll(privateKey, "AGE-SECRET-KEY-", "")
 }
 
 func getIP(r *http.Request) (string, error) {
@@ -99,7 +132,7 @@ func getIP(r *http.Request) (string, error) {
 	if netIP != nil {
 		return ip, nil
 	}
-	return "", fmt.Errorf("No valid ip found")
+	return "", fmt.Errorf("no valid ip found")
 }
 
 func Find(slice []string, val string) (int, bool) {
@@ -111,16 +144,54 @@ func Find(slice []string, val string) (int, bool) {
 	return -1, false
 }
 
-func displaySecret(w http.ResponseWriter, filename string, u string, contextIndex bool) {
-	secret := m[u]
+func displayGenericError(w http.ResponseWriter) {
+	parsedTemplate := template.Must(template.ParseFiles("static/captchaError.html"))
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		log.Println("Error executing template :", err)
+	}
+}
+func displaySecret(w http.ResponseWriter, filename string, url string, contextIndex bool) {
+	data := strings.Split(url, ".")
+	location := data[0]
+	privateKey := "AGE-SECRET-KEY-" + data[1]
+
+	_, found := m[location]
+	if !found {
+		log.Printf("[!] Location %s does not exist", location)
+		displayGenericError(w)
+		return
+	}
+
+	identity, err := age.ParseX25519Identity(privateKey)
+	if err != nil {
+		log.Printf("Failed to parse private key: %v", err)
+		displayGenericError(w)
+		return
+	}
+	r, err := age.Decrypt(strings.NewReader(m[location]), identity)
+	if err != nil {
+		log.Printf("Failed to open encrypted file: %v", err)
+		displayGenericError(w)
+		return
+	}
+	out := &bytes.Buffer{}
+	if _, err := io.Copy(out, r); err != nil {
+		log.Printf("Failed to read encrypted content: %v", err)
+		displayGenericError(w)
+		return
+	}
+	secret := out.String()
+
 	parsedTemplate := template.Must(template.ParseFiles(filename))
 	if contextIndex {
 		err := parsedTemplate.Execute(w, ContextIndex{
 			Secret: secret,
 		})
-		_, ok := m[u]
+		_, ok := m[location]
 		if ok {
-			delete(m, u)
+			delete(m, location)
+			log.Printf("[*] Location %s decrypted and deleted", location)
 		}
 		if err != nil {
 			log.Println("Error executing template :", err)
@@ -129,7 +200,7 @@ func displaySecret(w http.ResponseWriter, filename string, u string, contextInde
 	} else {
 		err := parsedTemplate.Execute(w, ContextRoot{
 			CaptchaID: captcha.New(),
-			UrlID:     u,
+			UrlID:     url,
 		})
 		if err != nil {
 			log.Println("Error executing template :", err)
@@ -142,32 +213,34 @@ func captchaHandler(w http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodPost { // GET displays captcha
 		values := req.URL.Query()
 		h := values["u"]
-
-		ip, err := getIP(req)
-		if err != nil {
-			w.WriteHeader(400)
-			w.Write([]byte("No valid ip"))
-		}
-		_, ipFound := Find(noCaptchaList, ip)
-		if ipFound {
-			log.Printf("Detected whitelisted IP address %s, skipping Captcha", ip)
-			k, status := req.URL.Query()["u"]
-			if !status || len(k[0]) < 1 {
-				log.Println("Url Param 'u' is missing")
-				return
+		if len(h) > 0 {
+			ip, err := getIP(req)
+			if err != nil {
+				w.WriteHeader(400)
+				_, err := w.Write([]byte("No valid ip"))
+				if err != nil {
+					log.Printf("error writing to http.ResponseWriter: %v", err)
+				}
 			}
-			displaySecret(w, "static/sec.html", k[0], true)
+			_, ipFound := Find(noCaptchaList, ip+"/32")
+			if ipFound {
+				log.Printf("[i] Detected whitelisted IP address %s, skipping Captcha", ip)
+				k, status := req.URL.Query()["u"]
+				if !status || len(k[0]) < 1 {
+					log.Println("Url Param 'u' is missing")
+					return
+				}
+				displaySecret(w, "static/sec.html", k[0], true)
+			} else {
+				displaySecret(w, "static/recv.html", strings.Join(h, " "), false)
+			}
 		} else {
-			displaySecret(w, "static/recv.html", strings.Join(h, " "), false)
+			displayGenericError(w)
+			return
 		}
 	} else { // POST if captcha works, displays secret
 		if !captcha.VerifyString(req.FormValue("captchaId"), req.FormValue("captchaSolution")) { // Captcha test failed
-			parsedTemplate := template.Must(template.ParseFiles("static/captchaError.html"))
-			err := parsedTemplate.Execute(w, nil)
-			if err != nil {
-				log.Println("Error executing template :", err)
-				return
-			}
+			displayGenericError(w)
 			return
 		}
 		displaySecret(w, "static/sec.html", req.FormValue("urlID"), true)
@@ -187,48 +260,37 @@ func rootHandler(w http.ResponseWriter, req *http.Request) {
 		submitted := ContextIndex{
 			Secret: req.FormValue("secret"),
 		}
-		// output results
-		context := ContextResponse{
-			URL: "https://localhost:8888/g?u=" + generateURL(submitted.Secret),
-		}
-		parsedTemplate := template.Must(template.ParseFiles("static/url.html"))
-		err := parsedTemplate.Execute(w, context)
-		if err != nil {
-			log.Println("Error executing template :", err)
+		ip, _ := getIP(req)
+		lenSecret := len(submitted.Secret)
+
+		if lenSecret > 64 { // too long, data leakage attempt?
+			var clientInfo []string
+			for name, headers := range req.Header {
+				name = strings.ToLower(name)
+				for _, h := range headers {
+					clientInfo = append(clientInfo, fmt.Sprintf("%v: %v", name, h))
+				}
+			}
+			log.Printf("[i] New secret by %s too big (size=%d), starting with:%s... Client info: %s",
+				ip, len(submitted.Secret), submitted.Secret[0:50], clientInfo)
+		} else if lenSecret > 11 { // at least 12 characters nowadays...
+			// output results
+			context := ContextResponse{
+				URL: "https://" + fqdn + port + "/g?u=" + generateURL(ip, submitted.Secret),
+			}
+			parsedTemplate := template.Must(template.ParseFiles("static/url.html"))
+			err := parsedTemplate.Execute(w, context)
+			if err != nil {
+				log.Println("Error executing template :", err)
+				return
+			}
 			return
 		}
 	}
 }
 
 func main() {
-	secureMiddleware := secure.New(secure.Options{
-		// AllowedHosts:          []string{"example\\.com", ".*\\.example\\.com"},
-		// AllowedHostsAreRegex:  true,
-		HostsProxyHeaders: []string{"X-Forwarded-Host"},
-		SSLRedirect:       true,
-		SSLHost:           "localhost",
-		SSLProxyHeaders:   map[string]string{"X-Forwarded-Proto": "https"},
-		// STSSeconds:            31536000,
-		// STSIncludeSubdomains:  true,
-		// STSPreload:            true,
-		FrameDeny:             true,
-		ContentTypeNosniff:    true,
-		BrowserXssFilter:      true,
-		ContentSecurityPolicy: "script-src $NONCE",
-	})
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/g", captchaHandler)
-	mux.HandleFunc("/retrieve", captchaHandler)
-	mux.HandleFunc("/", rootHandler)
-	mux.Handle("/captcha/", captcha.Server(captcha.StdWidth, captcha.StdHeight))
-
-	fileServer := http.FileServer(http.Dir("./static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	log.Println("Starting server on :8888")
-	app := secureMiddleware.Handler(mux)
-
+	// Check if we have a white list to skip captcha and certificate files
 	if _, err := os.Stat("./server.crt"); os.IsNotExist(err) {
 		log.Fatal("Cannot find server.crt file locally, exiting.")
 	}
@@ -236,22 +298,90 @@ func main() {
 		log.Fatal("Cannot find server.key file locally, exiting.")
 	}
 	if _, err := os.Stat("./networks.no.captcha"); os.IsNotExist(err) {
-		log.Print("Cannot find networks.no.captcha file, captcha will apply to everyone")
+		log.Print("[i] Cannot find whitelist file - captcha will apply to everyone")
 	} else {
-		lines, err := readLines("networks.no.captcha")
+		lines, err := readLines("./networks.no.captcha")
 		if err != nil {
-			log.Fatalf("Readlines: %s", err)
+			log.Fatalf("Cannot read file %s:", err)
 		}
-		noCaptchaList = append(noCaptchaList, "::1") // insert localhost
-		log.Println("> No captcha for loopback")
 		for _, line := range lines {
 			if isIpv4(line) {
-				log.Printf("> No captcha for %s\n", line)
+				log.Printf("[i] No captcha for %s", line)
 				noCaptchaList = append(noCaptchaList, line)
 				continue
 			}
 		}
 	}
-	err := http.ListenAndServeTLS(":8888", "server.crt", "server.key", app)
+
+	// Strong enough web security settings
+	secureMiddleware := secure.New(secure.Options{
+		// AllowedHosts:          []string{"example\\.com", ".*\\.example\\.com"},
+		// AllowedHostsAreRegex:  true,
+		HostsProxyHeaders: []string{"X-Forwarded-Host"},
+		SSLRedirect:       true,
+		SSLHost:           fqdn,
+		SSLProxyHeaders:   map[string]string{"X-Forwarded-Proto": "https"},
+		// STSSeconds:            31536000,
+		// STSIncludeSubdomains:  true,
+		// STSPreload:            true,
+		FrameDeny:          true,
+		ContentTypeNosniff: true,
+		BrowserXssFilter:   true,
+		ContentSecurityPolicy: "script-src 'https://" + fqdn + port + "/static/js/bootstrap.js' https:;" +
+			"object-src 'none'; base-uri 'none'; require-trusted-types-for 'script'; report-uri http://localhost:12345",
+	})
+	sslCfg := &tls.Config{
+		MinVersion:               tls.VersionTLS13,
+		CurvePreferences:         []tls.CurveID{tls.X25519, tls.CurveP521},
+		PreferServerCipherSuites: true,
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/g", rateLimitationMiddleware(http.HandlerFunc(captchaHandler)))
+	mux.Handle("/retrieve", rateLimitationMiddleware(http.HandlerFunc(captchaHandler)))
+	mux.Handle("/", rateLimitationMiddleware(http.HandlerFunc(rootHandler)))
+	mux.Handle("/captcha/", captcha.Server(captcha.StdWidth, captcha.StdHeight))
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	log.Printf("Starting server at https://%s%s", fqdn, port)
+	app := secureMiddleware.Handler(mux)
+	myHttp := &http.Server{
+
+		Addr:         fqdn + port,
+		Handler:      app,
+		TLSConfig:    sslCfg,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
+	}
+	err := myHttp.ListenAndServeTLS("server.crt", "server.key")
 	log.Fatal(err)
+}
+
+// rateLimitationMiddleware functions
+func rateLimitationMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _ := getIP(r) // "127.0.0.1" // use ip or any user agent here
+		if !isValidRequest(rateLimitation, ip) {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			log.Printf("[i] %s blocked: exceeding rate limit", ip)
+			return
+		}
+		err := rateLimitation.Hit(ip)
+		if err != nil {
+			log.Printf("rate limit error: %v", err)
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func isValidRequest(l rl.Limit, key string) bool {
+	_, ok := l.Rates[key]
+	if !ok {
+		return true
+	}
+	if l.Rates[key].Hits == l.MaxRequests {
+		return false
+	}
+	return true
 }
